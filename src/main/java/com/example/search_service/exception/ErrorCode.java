a45e9e844824393ffe70535a11fae7d1b136e905
Tran_Hoang_Vu <hoangvudx2004@gmail.com>
2025-10-27 21:59:20 +0700

    [implement] error code

---
+++ src/main/java/com/example/search_service/exception/ErrorCode.java
@@ -0,0 +1,22 @@
+package com.example.search_service.exception;
+
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+
+@Getter
+public enum ErrorCode {
+    PRODUCT_EXISTED(1001,"Product Id is existed",HttpStatus.BAD_REQUEST),
+    PRODUCT_NOT_EXISTED(1002,"Product Id is not existed",HttpStatus.BAD_REQUEST),
+
+    ;
+    private int code;
+    private String message;
+    private HttpStatusCode httpStatusCode;
+    ErrorCode(int code, String message, HttpStatusCode httpStatusCode) {
+        this.code = code;
+        this.message = message;
+        this.httpStatusCode = httpStatusCode;
+    }
+
+}
