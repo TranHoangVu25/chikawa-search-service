dcfeeb495cee845318a57c2e535e2ca9bc014c97
Tran_Hoang_Vu <hoangvudx2004@gmail.com>
2025-11-06 16:37:21 +0700

    [implement] model receive data

---
+++ src/main/java/com/example/search_service/models/ProductReceiveEvent.java
@@ -0,0 +1,26 @@
+package com.example.search_service.models;
+
+import com.example.search_service.Enums.Action;
+import com.example.search_service.dto.request.CategoryDTO;
+import com.example.search_service.dto.request.CharacterDTO;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ProductReceiveEvent {
+    private String id;
+    private String name;
+    private List<CategoryDTO> categories;
+    private List<CharacterDTO> characters;
+    private Double price;
+    private String status;
+    private List<String> images;
+    private Action action;
+}
